7d7f501d553be693f74109311765ab990d1c228c
alexsabdev <alexsabdev@gmail.com>
2015-12-23 16:02:20 +0300

    demo.js uncommented

--- source/scripts/demo.js
+++ source/scripts/demo.js
@@ -1,46 +1,44 @@
 'use_strict';
 
+angular.module('Demo', ['notification-service'])
 
+.controller('DemoController', ['$scope', 'Notification', function($scope, Notification){
 
-// angular.module('Demo', ['notification-service'])
+	$scope.notes = [];
+	$scope.note = {
+			id: '',
+			from: 'userManagement',
+			category: 'info',
+			header: 'Title Info',
+			content: 'Lorem ipsum dolor sit amet.',
+			type: 'note'
+		};
 
-// .controller('DemoController', ['$scope', 'Notification', function($scope, Notification){
+	$scope.useMockup = function() {
+		$scope.note = {
+			id: '',
+			from: 'userManagement',
+			category: 'info',
+			header: 'Title Info',
+			content: 'Lorem ipsum dolor sit amet.',
+			type: 'note'
+		};
+	};
 
-// 	$scope.notes = [];
-// 	$scope.note = {
-// 			id: '',
-// 			from: 'userManagement',
-// 			category: 'info',
-// 			header: 'Title Info',
-// 			content: 'Lorem ipsum dolor sit amet.',
-// 			type: 'note'
-// 		};
+	$scope.createNote = function() {
+		$scope.note.id = $scope.notes.length + 1;
+		Notification.notify($scope.note);
+		$scope.notes.push($scope.note);
+		//console.log('created a note with ID ' + $scope.note.id);
+		//console.log('array now has ' + $scope.notes.length + ' notes');
+		$scope.note = {
+			id: '',
+			from: 'userManagement',
+			category: '',
+			header: '',
+			content: '',
+			type: 'note'
+		};
+	};
 
-// 	$scope.useMockup = function() {
-// 		$scope.note = {
-// 			id: '',
-// 			from: 'userManagement',
-// 			category: 'info',
-// 			header: 'Title Info',
-// 			content: 'Lorem ipsum dolor sit amet.',
-// 			type: 'note'
-// 		};
-// 	};
-
-// 	$scope.createNote = function() {
-// 		$scope.note.id = $scope.notes.length + 1;
-// 		Notification.notify($scope.note);
-// 		$scope.notes.push($scope.note);
-// 		//console.log('created a note with ID ' + $scope.note.id);
-// 		//console.log('array now has ' + $scope.notes.length + ' notes');
-// 		$scope.note = {
-// 			id: '',
-// 			from: 'userManagement',
-// 			category: '',
-// 			header: '',
-// 			content: '',
-// 			type: 'note'
-// 		};
-// 	};
-
-// }]);
+}]);
