383189a28d7f1cc5354d4723f95bf836b760b7f3
alexsabdev <alexsabdev@gmail.com>
2015-12-23 18:50:12 +0300

    allocation

--- source/scripts/notification-service.js
+++ source/scripts/notification-service.js
@@ -6,78 +6,43 @@ angular.module('notification-service', [])
 
 	var options = {
 		delay: 3000,
-		limit: 2
+		limit: 3,
+		startX: 5,
+		startY: 5,
+		spacingY: 5
 	};
 
-	var onScreen = 0;
 	var buffer = [];
-
-	var appendElement = function(child, parent) {
-
-		angular.element(document).find(parent).append(child);
-		if (notificationBlockExists) {
-			onScreen++;
-			console.log("on screen: " + onScreen);
-		}
-
-	};
-
-	var notificationBlockExists = false;
-
-	this.notify = function(note) {
-		if (!notificationBlockExists) {
-			appendElement('<notificationBlock></notificationBlock>', 'body');
-			notificationBlockExists = true;
-		}
-		var noteDiv = 
-		'<div class="alert alert-'
-		+ note.category
-		+ ' alert-dismissible notification" role="alert" id="'
-		+ note.id
-		+ '"><strong>'
-		+ note.header
-		+ ' '
-		+ note.id
-		+ '</strong> <br />'
-		+ note.content
-		+ '</div>';
-		note.noteDiv = noteDiv;
-
-		if (onScreen === options.limit) {
-			var toBuffer = document.getElementsByTagName('notificationBlock')[0].firstChild;
-			buffer.push(toBuffer);
-			toBuffer.remove();
-			onScreen--;
-			console.log(buffer);
-		}
-
-		if (onScreen < options.limit) {
-
-			appendElement(note.noteDiv, 'notificationBlock');
-
-			setTimeout(function() {
-				if (document.getElementById(note.id) !== null) {
-					document.getElementById(note.id).remove();
-					onScreen--;
-					// if (buffer.length !== 0) {
-					// 	appendElement(buffer[0], 'notificationBlock');
-					// 	buffer.slice(0,1);
-					// 	onScreen++;
-					// }
-
-				}
-			},options.delay);
-
-			document.getElementById(note.id).addEventListener("click", function() {
-				onScreen--;
-				clearTimeout(this);
-				this.remove();
-				console.log("killed");
-			});
-		}
-	};
-
 	
 
-});
+	var allocate = function() {
+		var i = options.startX;
+		var j = options.startY;
 
+
+
+		for (var n in buffer) {
+
+
+
+				buffer[n].style.right = i + 'px';
+				buffer[n].style.top = j + 'px';
+				buffer[n].style.visibility = 'visible';
+				j += buffer[n].offsetHeight + options.spacingY;
+			
+		}
+	};
+
+	this.notify = function(note) {
+		var templateNote = document.createElement('div');
+		templateNote.className = 'alert alert-' + note.category + ' alert-dismissible notification';
+		var att = document.createAttribute('role'); att.value = 'alert'; templateNote.setAttributeNode(att);
+		var attId = document.createAttribute('id'); attId.value = note.id; templateNote.setAttributeNode(attId);
+		templateNote.innerHTML = '<strong>' + note.header + ' ' + note.id + '</strong> <br />' + note.content;
+		templateNote.style.visibility = 'hidden';
+		document.getElementsByTagName('body')[0].appendChild(templateNote);
+		buffer.push(templateNote);
+		allocate();
+	};
+
+});
