904d44bc9c87dcb0513ef3fba57ce290dc8a83d5
alexsabdev <alexsabdev@gmail.com>
2015-12-24 20:07:55 +0300

    half

--- gruntfile.js
+++ gruntfile.js
@@ -31,11 +31,70 @@ module.exports = function(grunt) {
 		},
 		ngdocs: {
 			all: ['source/scripts/notification-service.js', 'source/scripts/demo.js']
+		},
+		jshint: {
+			options: {
+				"curly": true,
+				"eqnull": true,
+				"eqeqeq": true,
+				"undef": true,
+				"latedef": true,
+				"newcap": true,
+				"noarg": true,
+				"sub": true,
+				"browser": true,
+				"globals": {
+					"jQuery": true,
+					$: true,
+					console: true
+				}
+			},
+			'<%= pkg.name %>': {
+				src: ['source/scripts/*.js']
+			}
+		},
+		concat: {
+			dist: {
+				src: ['source/scripts/notification-service.js', 'source/scripts/demo.js'],
+				dest: 'demo/scripts/main.js'
+			}
+		},
+		uglify: {
+			options: {
+				stripBanners: true,
+				banner: '/* <%= pkg.name %> -v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+			},
+			build: {
+				src: 'demo/scripts/main.js',
+				dest: 'demo/scripts/main.js'
+			}
+		},
+		cssmin: {
+			with_banner: {
+				options: {
+					banner: '/* My minified CSS */'
+				},
+				files: { 'demo/style/main.min.css' : ['source/styles/notification-service-styles.css']
+				}
+			}
+		},
+		removeLogging: {
+			build: {
+				src: 'demo/scripts/main.js',
+				dest: 'demo/scripts/main.min.js'
+			}
 		}
+
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-express');
 	grunt.loadNpmTasks('grunt-ngdocs');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-remove-logging');
 	grunt.registerTask('default', ['express', 'watch']);
+	grunt.registerTask('demo', ['concat','uglify','cssmin']);
 };
