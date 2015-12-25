6f2472f74b78d914c439aa22273777c0470d89a9
alexsabdev <alexsabdev@gmail.com>
2015-12-25 09:12:18 +0300

    added handler of incorrect input data

--- source/scripts/notification-service.js
+++ source/scripts/notification-service.js
@@ -53,6 +53,15 @@ angular.module('notification-service', [])
 	};
 	// procedure invoked when an alert is called
 	this.notify = function(note) {
+		// handling incorrect input data
+		if (!note.id) 
+			note.id = 'und' + (new Date).getTime();
+		if (!note.category)
+			note.category = 'error';
+		if (!note.header)
+			note.header = 'NO HEADER';
+		if (!note.content)
+			note.content = 'NO CONTENT';
 		// create a template for an alert
 		var templateNote = document.createElement('div');
 		templateNote.className = 'ns-alert-' + note.category + ' notification';
