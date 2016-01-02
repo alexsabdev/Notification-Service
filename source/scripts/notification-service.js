421c44be68f13dcbdd381f6631fe632feccfab8d
alexsabdev <alexsabdev@gmail.com>
2016-01-02 21:16:46 +0300

    working new version

--- source/scripts/notification-service.js
+++ source/scripts/notification-service.js
@@ -1,99 +1,72 @@
-'use_strict';
 /**
  * noficication-service
  * @author alexsabdev (alexsabdev@gmail.com)
  * @version v1.0.0
  * @link https://github.com/alexsabdev/notification-service.git
  */
-angular.module('notification-service', [])
+var nsModule = angular.module('notification-service', []);
 /**
- * @ngdoc service
- * @name notification-service.service:Notification
- * @description 
- *
- * notification-service - Angular.js service providing notifications using Bootstrap 3 styles with css transition animation. It creates overlay notifications on window. Number of notifications is limited by options.limit. When the limit is reached, the oldest notification temporary gets off from screen and the latest pops up. And after that gets back when there's a free spot. Created notifications are closable by clicking. They are also selfclosed within period of options.delay.
- * <strong>Checkout README.MD for detailed instructions!</strong>
- * 
- */
-.service('Notification', function(){
-	// //object to collect options of the module service
-	// var options = {
-	// 	delay: 90000,
-	// 	limit: 5,
-	// 	startX: 5,
-	// 	startY: 5,
-	// 	spacingY: 5
-	// };
-	// // array to keep track of called notifications
-	// var buffer = [];
-	// //variable helping to display latest notification
-	// var shift = 0;
-	// //procedure to display latest notifications
-	// var allocate = function() {
-	// 	// variables to set starting position for alerts
-	// 	var i = options.startX;
-	// 	var j = options.startY;
-	// 	// loop to clean window before refreshing
-	// 	for (var k in buffer) {
-	// 		buffer[k].style.right = options.startX + 'px';
-	// 		buffer[k].style.top = options.startY + 'px';
-	// 		buffer[k].style.visibility = 'hidden';
-	// 	}
-	// 	// condition to adjust the shift variable
-	// 	if (buffer.length >= options.limit) {
-	// 		shift = buffer.length - options.limit;
-	// 	}
-	// 	// loop to locate latest alerts on window
-	// 	for (var n = shift; n < buffer.length; n++) {
-	// 		buffer[n].style.right = i + 'px';
-	// 		buffer[n].style.top = j + 'px';
-	// 		buffer[n].style.visibility = 'visible';
-	// 		j += buffer[n].offsetHeight + options.spacingY;
-	// 	}
-	// };
-	// // procedure invoked when an alert is called
-	// this.notify = function(note) {
-	// 	// handling incorrect input data
-	// 	if (!note.id)
-	// 		note.id = 'und' + (new Date).getTime();
-	// 	if (!note.header)
-	// 		note.header = 'NO HEADER';
-	// 	if (!note.content)
-	// 		note.content += 'NO CONTENT';
-	// 	if (!note.category) {
-	// 		note.category = 'error';
-	// 		note.content = 'NO CATEGORY ' + note.content;
-	// 	}
-	// 	// create a template for an alert
-	// 	var templateNote = document.createElement('div');
-	// 	templateNote.className = 'ns-alert-' + note.category + ' notification';
-	// 	var attId = document.createAttribute('id'); attId.value = note.id; templateNote.setAttributeNode(attId);
-	// 	templateNote.innerHTML = '<strong>' + note.header + '</strong> <br />' + note.content;
-	// 	templateNote.style.visibility = 'hidden';
-	// 	// append the template to DOM
-	// 	document.getElementsByTagName('body')[0].appendChild(templateNote);
-	// 	// adding an eventlistener closing the alert when clicked
-	// 	templateNote.addEventListener('click', function() {
-	// 		buffer.splice(buffer.indexOf(this), 1);
-	// 		allocate();
-	// 		this.remove();
-	// 		this.removeEventListener('click');
-	// 	});
-	// 	// setting a timeout for closing the aired alert
-	// 	setTimeout(function() {
-	// 		var target = document.getElementById(note.id);
-	// 		if (target) {
-	// 			buffer.splice(buffer.indexOf(target), 1);
-	// 			allocate();
-	// 			target.remove();
-	// 			target.removeEventListener('click');
-	// 		}
-	// 	}, options.delay);
-	// 	// adding created alert to the array
-	// 	buffer.push(templateNote);
-	// 	// showing latest alerts
-	// 	allocate();
-	// };
-	
-	
+* @ngdoc service
+* @name notification-service.service:Notification
+* @description 
+*
+* notification-service - Angular.js service providing notifications using Bootstrap 3 styles with css transition animation. It creates overlay notifications on window. Number of notifications is limited by options.limit. When the limit is reached, the oldest notification temporary gets off from screen and the latest pops up. And after that gets back when there's a free spot. Created notifications are closable by clicking. They are also selfclosed within period of options.delay.
+* <strong>Checkout README.MD for detailed instructions!</strong>
+* 
+*/
+nsModule.service('Notification', function($rootScope, $compile){
+	// scope for the service
+	var scope = $rootScope.$new();
+	// object for the service options
+	scope.options = {
+		delay: 90000,
+		limit: 5
+	};
+	// array to keep track of incoming alerts
+	scope.buffer = [];
+	// variable to keep track if the notification area is displayed or not
+	scope.isDisplayed = false;
+	// procedure to kill the alert
+	scope.kill = function(item) {
+		scope.buffer.splice(scope.buffer.indexOf(item), 1);
+		console.log(scope.buffer.length);
+		if (scope.buffer.length === 0) {
+			scope.isDisplayed = false;
+			angular.element(document.getElementById('notification-area')).remove();
+		}
+	};
+	// procedure to send the user response
+	scope.sendResponse = function(item, answer) {
+		console.log(item.id);
+		console.log(answer);
+	};
+	// procedure to set new options
+	this.setOptions = function(opts) {
+		if (!angular.isObject(opts)) throw new Error("Options should be an object!");
+		scope.options = angular.extend({}, scope.options, opts);
+	};
+	// procedure to air the alert
+	this.notify = function(note) {
+		scope.buffer.push(note);
+		if (!scope.isDisplayed) {
+			var template =
+			'<ul id="notification-area">' +
+			'<li id="ns-{{item.id}}" class="ns-alert-{{item.category}} notification" ng-repeat="item in buffer|limitTo:-options.limit">' +
+			'<span class="ns-close" ng-click="kill(item)">&times;</span>' +
+			'<p class="ns-title"><strong>{{item.header}} {{item.id}}</strong></p>' +
+			'<p class="ns-content">{{ item.content }}</p>' +
+			'<button class="ns-btn ns-btn-xs ns-btn-{{item.category}}" ng-show="item.type!==\'note\'" ng-click="sendResponse(item,1)">OK</button>' +
+			'<button class="ns-btn ns-btn-xs ns-btn-{{item.category}}" ng-show="(item.type!==\'note\')&&(item.type!==\'ok_confirm\')" ng-click="sendResponse(item,0)">Cancel</button>' +
+			'</li></ul>';
+			angular.element(document.getElementsByTagName('body')).append($compile(template)(scope));
+			scope.isDisplayed = true;
+		}
+		window.setTimeout(function() {
+			var toKill = angular.element(document.getElementById('ns-' + note.id))[0];
+			if (toKill) {
+				toKill.remove();
+				scope.buffer.splice(scope.buffer.indexOf(note),1);
+			}
+		},scope.options.delay);
+	};
 });
