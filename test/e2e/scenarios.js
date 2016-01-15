e4f623b2b8b34dd3c0326564da8eaec6decc7697
alexsabdev <alexsabdev@gmail.com>
2016-01-15 23:32:45 +0300

    updated tests reflecting the demo modal

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -14,7 +14,7 @@ describe('E2E: notification-service', function() {
 			element(by.model('note.content')).sendKeys('Content');
 			element.all(by.model('note.category')).get(0).click();
 			element.all(by.model('note.type')).get(0).click();
-			element(by.id('simulateButton')).click();	
+			element(by.id('simulateButton')).click();
 		});
 
 		it('should display an alert', function() {
@@ -28,12 +28,12 @@ describe('E2E: notification-service', function() {
 
 		it('should close the alert on pressing the x span', function() {
 			element(by.css('.ns-close')).click();
-			expect(element(by.css('li')).isPresent()).toBeFalsy();
+			expect(element(by.css('.notification')).isPresent()).toBeFalsy();
 		});
 
 		it('should automatically close the alert in 90 sec', function() {
 			setTimeout(function() {
-				expect(element(by.css('li')).isPresent()).toBeFalsy();
+				expect(element(by.css('.notification')).isPresent()).toBeFalsy();
 			}, 90000);
 		});
 	});
@@ -122,94 +122,99 @@ describe('E2E: notification-service', function() {
 
 	describe('talking to server', function() {
 
-		// beforeEach(function() {
-		// 	browser.ignoreSynchronization = false;
-		// 	browser.addMockModule('httpBackendMock', function() {
-		// 		angular.module('httpBackendMock', ['Demo', 'notification-service', 'ngMockE2E'])
-		// 		.run(function($httpBackend) {
+		beforeEach(function() {
+			// 	browser.ignoreSynchronization = false;
+			// 	browser.addMockModule('httpBackendMock', function() {
+			// 		angular.module('httpBackendMock', ['Demo', 'notification-service', 'ngMockE2E'])
+			// 		.run(function($httpBackend) {
 
-		// 			var list =
-		// 			[
-		// 			{
-		// 				"id": 100,
-		// 				"from": "userManagement",
-		// 				"category": "info",
-		// 				"type": "note",
-		// 				"header": "Lorem ipsum dolor.",
-		// 				"content": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi, quae."
-		// 			},
-		// 			{
-		// 				"id": 101,
-		// 				"from": "userManagement",
-		// 				"category": "warning",
-		// 				"type": "ok_confirm",
-		// 				"header": "Lorem ipsum dolor sit.",
-		// 				"content": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex dolorem, maiores."
-		// 			},
-		// 			{
-		// 				"id": 102,
-		// 				"from": "userManagement",
-		// 				"category": "error",
-		// 				"type": "ok_cancel_confirm",
-		// 				"header": "Lorem ipsum dolor sit.",
-		// 				"content": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex dolorem, maiores."
-		// 			},
-		// 			{
-		// 				"id": 103,
-		// 				"from": "userManagement",
-		// 				"category": "info",
-		// 				"type": "ok_confirm",
-		// 				"header": "Lorem ipsum.",
-		// 				"content": "Lorem ipsum dolor sit amet."
-		// 			},
-		// 			{
-		// 				"id": 104,
-		// 				"from": "userManagement",
-		// 				"category": "warning",
-		// 				"type": "ok_cance_confirm",
-		// 				"header": "Lorem ipsum dolor sit.",
-		// 				"content": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex dolorem, maiores."
-		// 			},
-		// 			{
-		// 				"id": 105,
-		// 				"from": "userManagement",
-		// 				"category": "error",
-		// 				"type": "info",
-		// 				"header": "Lorem ipsum dolor sit.",
-		// 				"content": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse ad harum architecto? Accusantium!"
-		// 			},
-		// 			{
-		// 				"id": 106,
-		// 				"from": "userManagement",
-		// 				"category": "warning",
-		// 				"type": "info",
-		// 				"header": "Lorem ipsum dolor sit.",
-		// 				"content": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit, sint."
-		// 			}
-		// 			];
+			// 			var list =
+			// 			[
+			// 			{
+			// 				"id": 100,
+			// 				"from": "userManagement",
+			// 				"category": "info",
+			// 				"type": "note",
+			// 				"header": "Lorem ipsum dolor.",
+			// 				"content": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi, quae."
+			// 			},
+			// 			{
+			// 				"id": 101,
+			// 				"from": "userManagement",
+			// 				"category": "warning",
+			// 				"type": "ok_confirm",
+			// 				"header": "Lorem ipsum dolor sit.",
+			// 				"content": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex dolorem, maiores."
+			// 			},
+			// 			{
+			// 				"id": 102,
+			// 				"from": "userManagement",
+			// 				"category": "error",
+			// 				"type": "ok_cancel_confirm",
+			// 				"header": "Lorem ipsum dolor sit.",
+			// 				"content": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex dolorem, maiores."
+			// 			},
+			// 			{
+			// 				"id": 103,
+			// 				"from": "userManagement",
+			// 				"category": "info",
+			// 				"type": "ok_confirm",
+			// 				"header": "Lorem ipsum.",
+			// 				"content": "Lorem ipsum dolor sit amet."
+			// 			},
+			// 			{
+			// 				"id": 104,
+			// 				"from": "userManagement",
+			// 				"category": "warning",
+			// 				"type": "ok_cance_confirm",
+			// 				"header": "Lorem ipsum dolor sit.",
+			// 				"content": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex dolorem, maiores."
+			// 			},
+			// 			{
+			// 				"id": 105,
+			// 				"from": "userManagement",
+			// 				"category": "error",
+			// 				"type": "info",
+			// 				"header": "Lorem ipsum dolor sit.",
+			// 				"content": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse ad harum architecto? Accusantium!"
+			// 			},
+			// 			{
+			// 				"id": 106,
+			// 				"from": "userManagement",
+			// 				"category": "warning",
+			// 				"type": "info",
+			// 				"header": "Lorem ipsum dolor sit.",
+			// 				"content": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit, sint."
+			// 			}
+			// 			];
 
-		// 			$httpBackend.whenGET('http://localhost:3000/list').respond(list);
+			// 			$httpBackend.whenGET('http://localhost:3000/list').respond(list);
 
-		// 		    // $httpBackend.whenPOST('http://localhost:3000/confirm').respond(function(method, url, data) {
-		// 		    //   var respond = angular.fromJson(data);
-		// 		    //   confirm.push(respond);
-		// 		    //   return [200, confirm, {}];
-		// 		    // });
-		// 		});
-		// 	});
-		// });
+			// 		    // $httpBackend.whenPOST('http://localhost:3000/confirm').respond(function(method, url, data) {
+			// 		    //   var respond = angular.fromJson(data);
+			// 		    //   confirm.push(respond);
+			// 		    //   return [200, confirm, {}];
+			// 		    // });
+			// 		});
+			// 	});
+			element(by.id('showModal')).click();
+			browser.sleep(1000);
+			element(by.id('getFromServer')).click();
+			element(by.css('button.close')).click();
+			browser.sleep(1000);
+		});
 
 		it('should fetch data from server', function() {
-			element(by.id('getFromServer')).click();
 			expect(element.all(by.repeater('item in buffer')).count()).toEqual(5);
 		});
 
 		it('should send response to server', function() {
-			element(by.id('getFromServer')).click();
-			element.all(by.css('li button')).first().click();
+			element.all(by.css('button.ns-btn')).first().click();
 			browser.get('http://localhost:3000/confirm/102');
 			expect(element(by.css('pre')).getText()).toContain('"id": 102');
 		});
 	});
+
+
 
 });
